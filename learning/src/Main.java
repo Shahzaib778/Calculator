eeb564724b798b8196d95234919e55afddd4f5f9
Shahzaib778 <116464265+Shahzaib778@users.noreply.github.com>
2024-04-23 01:55:23 +0500

    Add files via upload

--- learning/src/Main.java
+++ learning/src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.text.NumberFormat;
 import java.util.Date;
 import java.util.Scanner;
@@ -7,31 +8,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
+        int Loan = 0;
+        float interestRate = 0;
+        int numberOfPayment = 0;
+        float monthlyInterest = 0;
+        System.out.println("Requested Loan Amount $1000 - $1M:");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Requested Loan Amount: ");
-        int Loan = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter: ");
+            Loan = scanner.nextInt();
+            if (Loan >= 1000 && Loan <= 1000000)
+                break;
+            System.out.println("Enter Valid Value");
 
-        System.out.print("Requested Loan Interest Rate: ");
-        float interestRate = scanner.nextFloat();
-        float monthlyInterest = interestRate / 100 / 12;
+        }
 
-        System.out.print("Time For Loan Amount: ");
-        byte years = scanner.nextByte();
-        int numberOfPayment = years * 12;
 
-        double mortage = Loan * (monthlyInterest * Math.pow(1+monthlyInterest,numberOfPayment)
-                /(Math.pow(1+monthlyInterest,numberOfPayment)-1) );
+        System.out.println("Requested Loan Interest Rate:");
+        while (true) {
+            System.out.print("Enter: ");
+            interestRate = scanner.nextFloat();
+                monthlyInterest = interestRate / 100 / 12;
+            if (interestRate >= 1 && interestRate <= 30)
+                break;
+            System.out.println("Enter Valid Value");
+        }
+
+
+        System.out.println("Time For Loan Amount: ");
+        while (true) {
+            System.out.print("Enter: ");
+            byte years = scanner.nextByte();
+            numberOfPayment = years * 12;
+            if (numberOfPayment >= 1 && numberOfPayment <= 360)
+                break;
+            System.out.println("Enter Valid Value");
+        }
+
+        double mortage = Loan * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayment)
+                / (Math.pow(1 + monthlyInterest, numberOfPayment) - 1));
 
         String mortageFormated = NumberFormat.getCurrencyInstance().format(mortage);
-        System.out.println("Mortage: "+ mortageFormated);
-
-
-
-
+        System.out.println("Mortage: " + mortageFormated);
 
 
     }
-
-
 }
